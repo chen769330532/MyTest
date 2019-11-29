bf333ce0335713182771eec62f5141e8bd09a212
chenjl <chenjl@kamfu.com.cn>
2019-11-29 18:12:57 +0800

    增加lambda 案例

---
+++ demo/src/main/java/com/example/demo/testLambda/Function2.java
@@ -0,0 +1,7 @@
+package com.example.demo.testLambda;
+
+public interface Function2 {
+
+	public void add(String abc);
+	
+}
---
+++ demo/src/main/java/com/example/demo/testLambda/FunctionEntity.java
@@ -0,0 +1,11 @@
+package com.example.demo.testLambda;
+
+import lombok.Data;
+
+@Data
+public class FunctionEntity {
+
+	private String name ;
+	
+	private String id;
+}
