38228cd48d963858049a34b480509e7b12f53aae
chenjl <chenjl@kamfu.com.cn>
2020-01-15 09:51:32 +0800

    增加slf4j日志

--- demo/src/main/java/com/example/serviceImpl/MysqlServiceImpl.java
+++ demo/src/main/java/com/example/serviceImpl/MysqlServiceImpl.java
@@ -5,12 +5,14 @@ import com.example.pojo.Dept;
 import com.example.pojo.Emp;
 import com.example.pojo.Salgrade;
 import com.example.service.MysqlService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class MysqlServiceImpl implements MysqlService {
 
 	@Autowired
@@ -37,6 +39,8 @@ public class MysqlServiceImpl implements MysqlService {
 	@Override
 	public List<Emp> job(String job) {
 		List<Emp> list = null;
+		log.info("job  info的值:"+job);
+		log.debug("job  debug的值:"+job);
 		try {
 			list = mapper.job(job);
 		} catch (Exception e) {
