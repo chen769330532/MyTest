bf965c068955db30cbd100bb33a0a1531d33ad93
chenjl <chenjl@kamfu.com.cn>
2020-06-24 11:44:08 +0800

    测试拦截器

---
+++ demo/src/main/java/com/example/interceptor/MyHandlerInterceptor.java
@@ -0,0 +1,32 @@
+package com.example.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Chenjl
+ * @date 2020/6/9 10:47
+ */
+@Component
+public class MyHandlerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("执行preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("执行postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("执行afterCompletion");
+    }
+}
